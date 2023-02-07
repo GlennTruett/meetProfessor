4905a58f5958b6682436cbe22a650eaa6ed2d179
Glenn Truett <112903356+GlennTruett@users.noreply.github.com>
2023-02-07 15:33:28 -0700

    Add files via upload

--- meet Professor.cpp
+++ meet Professor.cpp
@@ -11,19 +11,21 @@
 using namespace std;
 
 
-char again; 
+int sound();
 
 int main()
-	
-	{
+{
+	char again = 'n';
+
 	do {
 		srand(static_cast<unsigned int>(time(NULL))); //seed random number generator
 		int randomNumber = rand(); //generate random number
-		int prof = (randomNumber % 10) + 1; //get a number between 1 and 10
+		int pnum = (randomNumber % 10) + 1; //get a number between 1 and 10
 		int choice;
 		string fName;
+		
 
-		cout << "Your random number is: " << prof << endl;
+		cout << "Your random number is: " << pnum << endl;
 		Sleep(2000);
 		cout << "What is your first name? \n"; // prints out the question What is your name?
 		cin >> fName; // receives the input from the user and assigns it to the variable names fName.
@@ -36,42 +38,50 @@ int main()
 		string Professor[10] = { "Craig Belanger", "Dr. David Brokaw","Dr. Brendly Clark Singleton","Rawad Habib","Matthew Marquit","Jake Perrine","Heather Peters","Dr. Daniel Pike","Matthew Prater","Dr. Mark Smith" };
 
 		for (int i = 0; i < 10; i++) {
-			cout << i << " " << Professor[i] << ", \n";
+			cout << i+1 << " " << Professor[i] << ", \n";
 		}
-		system("pause\n");
-		cout << "\nIf you will notice there are numbers in front of each professor's name. Please enter a number\n from 0 through 9 to guess who you will be meeting.\n";
-		cin >> choice;
+			system("pause\n");
+			cout << "\nIf you will notice there are numbers in front of each professor's name. Please enter a number\n from 1 through 10 to guess who you will be meeting.\n";
+			cin >> choice;
+			//choice--;
 
-		if (choice == prof) {
-			cout << "You guessed correctly\n The professor you will be meeting is " << Professor[prof] << endl;
-			int extra();
-		}
-		else {
-			cout << "\nSorry you guessed incorrectly\n";
-			cout << "\nWould you like to try again?\n Please enter y or n \n";
-			cin >> again;
-			if (again == 'n'){
-				cout << "\nHave a Nice Day! " << fName << endl;
+			if (choice-- == pnum) {
+				cout << "You guessed correctly\n" << "\nThe professor you will be meeting is " << Professor[pnum] << endl;
+				sound();
+				exit(0);
+
 			}
-		}
+			else {
+				cout << "\nSorry you guessed incorrectly\n";
+				cout << "\nWould you like to try again?\n Please enter y or n \n";
+				cin >> again;
+				if (again == 'n') {
+					cout << "\nHave a Nice Day! " << fName << endl;
 
-	}while (again == 'y');
-	
-	int extra(); {
+
+				}
+			}
+
+		
+		
+
+	} while (again == 'y');
+
+	return 0;
+}
+
+		int sound() {
 		Sleep(1000);
-		cout << "Your professor is late and you hear a sound." << endl;
+		cout << "\nYour professor is late and you hear a sound.\n" << endl;
 		Sleep(1000);
 		Beep(500, 1000);
 		Sleep(2000);
-		cout << "This sound means that class is done for the day and you can go home."<<endl;
+		cout << "This sound means that class is done for the day and you can go home.\n" << endl;
 		Sleep(1000);
-		cout << "HAVE A NICE DAY!";
+		cout << "HAVE A NICE DAY!"<< endl; 
 		Sleep(2000);
-
-
-	}
+	
 		return 0;
-}
+	}
 
-
-
+	
